10244e96980d4e21978613a2c48bf2654c3eb9cf
mrfelopes <feliperfragoso@gmail.com>
2024-02-03 22:41:04 -0300

    Estilização e título padrão adicionado

--- src/screens/SearchManga.jsx
+++ src/screens/SearchManga.jsx
@@ -9,36 +9,50 @@ export default function SearchManga() {
   const URLpadrao = 'https://api.mangadex.org';
   const [manga, setManga] = useState([]);
   const [mangaImgs, setMangaImgs] = useState([]);
-  const {width, height} = useWindowDimensions();
+  const { width, height } = useWindowDimensions();
+  const {updateImage, setUpdateImage} = useState(0);
+  const caracterePadrao = 'A';
 
   useEffect(() => {
-    if (!title) return;
-    if (title.length <= 1 && manga) {
-      setManga([]);
-      setMangaImgs([]);
-      return;
-    }
     const getManga = async () => {
       try {
-        const resposta = await axios.get(`${URLpadrao}/manga?title=${title}`);
+        if (title.length < 1){
+          const resposta = await axios.get(`${URLpadrao}/manga?title=${caracterePadrao}`);
         setManga(resposta.data.data);
 
         const coverIds = resposta.data.data.map((manga) =>
           manga.relationships.find((rel) => rel.type === 'cover_art')?.id
         );
-        console.log('Cover IDs:', coverIds);
 
         const imgResponses = await Promise.all(
           coverIds.map(async (coverId) => {
             if (coverId) {
               const coverRes = await axios.get(`${URLpadrao}/cover/${coverId}`);
-              console.log("DEBUG: ", coverRes.data.data)
               return coverRes.data.data.attributes.fileName;
             }
           })
         );
         setMangaImgs(imgResponses);
         console.log(imgResponses);
+        } else {;
+        const resposta = await axios.get(`${URLpadrao}/manga?title=${title}`);
+        setManga(resposta.data.data);
+
+        const coverIds = resposta.data.data.map((manga) =>
+          manga.relationships.find((rel) => rel.type === 'cover_art')?.id
+        );
+
+        const imgResponses = await Promise.all(
+          coverIds.map(async (coverId) => {
+            if (coverId) {
+              const coverRes = await axios.get(`${URLpadrao}/cover/${coverId}`);
+              return coverRes.data.data.attributes.fileName;
+            }
+          })
+        );
+        setMangaImgs(imgResponses);
+        console.log(imgResponses);
+        }
       } catch (error) {
         console.error("Erro ao obter dados do MangaDex:", error);
       }
@@ -46,39 +60,40 @@ export default function SearchManga() {
     getManga();
   }, [title]);
 
-  const flatlistColumnStyle = 
-    width >= 600
-      ? { columnWrapperStyle: {flex: 1, justifyContent: 'space-around', minWidth: width} }
-      : {};
-
   return (
     <View style={styles.fullBody}>
       <Text style={styles.textBigBold}>Lista de Mangás:</Text>
+      <TextInput
+        placeholder="Digite o título do mangá."
+        value={title}
+        style={styles.textInput}
+        onChangeText={(text) => setTitle(text)}
+      />
       <FlatList
-        style={styles.flatlistM}
+        style={{...styles.flatlistM,  flex: 1, minWidth: width }}
         data={manga}
         keyExtractor={(item) => item.id}
         renderItem={({ item, index }) => (
           <View style={styles.container}>
-            <Text style={styles.textSmallBold}>{item.attributes.title.en}</Text>
-            {mangaImgs[index] && (
-              <View style={styles.mangaView}>
-                <Image
-                  style={styles.mangaImgs}
-                  source={{ uri: `https://uploads.mangadex.org/covers/${item.id}/${mangaImgs[index]}` }}
-                />
-              </View>
+            <View style ={styles.containerManga}>
+              <Text style={styles.textSmallBold}>{item.attributes.title.en}</Text>
+              <Text style={styles.textSmall}>Status: {item.attributes.status}</Text>
+              {mangaImgs[index] && (
+                <View style={styles.mangaView}>
+                  <Image
+                    style={styles.mangaImgs}
+                    source={{ uri: `https://uploads.mangadex.org/covers/${item.id}/${mangaImgs[index]}` }}
+                    onError={(error) =>
+                      setUpdateImage(updateImage + 1)
+                    }
+                  />
+                  </View>
             )}
+            </View>
           </View>
         )}
-        {...flatlistColumnStyle}
-        numColumns={width >= 600 ? 2 : 1}
+        numColumns={1}
         key={width >= 600 ? 2 : 1}
-      />
-      <TextInput
-        label="Digite o título do mangá"
-        value={title}
-        onChangeText={(text) => setTitle(text)}
       />
     </View>
   );
