0f0c19534f38ee4b913b2e41ede305b0fe01ffb9
mrfelopes <feliperfragoso@gmail.com>
2024-02-04 12:32:04 -0300

    Added a 10 maximum image request limit to avoid API temporary block

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,21 +1,13 @@
 import {View} from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { styles } from '../utils/styles';
+import { useState } from 'react';
 
 export default function Home({ navigation }) {
+
   return (
-    <View style={styles.fullBody}>
-        <View style={styles.container}>
-        <Text style={styles.textBigBold}>Bem-Vindo(a) ao projeto de app para o MangaDex</Text>
-        <Button
-          mode="contained"
-          onPress={() => navigation.navigate('ShowManga')}
-          style={styles.button}
-        >
-          Teste
-        </Button>
+      <View style={styles.fullBody}>
+          <Text style={styles.textBigBold}>Welcome to (currently) MangaDex_App!</Text>
       </View>
-    </View>
-  
-  );
+    );
 }
